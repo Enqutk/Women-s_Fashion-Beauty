c1d4c50d72d6ead706d909c417bab74f1a03044e
Enqutk <enkukokob@gmail.com>
2026-04-26 14:42:22 +0300

    Add order tables initialization in server.ts to ensure proper setup of order management functionality during server startup.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import { ensureCartItemsTable } from "./modules/cart/cart.repository";
 import { checkDatabaseConnection } from "./config/db";
 import { ensureCategoriesTable } from "./modules/category/category.repository";
+import { ensureOrdersTables } from "./modules/order/order.repository";
 import { ensureProductsTable } from "./modules/product/product.repository";
 import { ensureUsersTable } from "./modules/user/user.repository";
 import { env } from "./config/env";
@@ -13,6 +14,7 @@ async function startServer(): Promise<void> {
     await ensureCategoriesTable();
     await ensureProductsTable();
     await ensureCartItemsTable();
+    await ensureOrdersTables();
     app.listen(env.port, () => {
       // Keep startup log concise for sprint 0 verification.
       console.log(`API server listening on http://localhost:${env.port}`);
