7751dc188d7959817ebc54e88a03ecea5935f1ad
Enqutk <enkukokob@gmail.com>
2026-04-26 14:30:17 +0300

    Add index file in product module to re-export routes, controller, service, and repository, improving import organization and module structure.

---
+++ server/src/modules/product/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./product.routes";
+export * as productController from "./product.controller";
+export * as productService from "./product.service";
+export * as productRepository from "./product.repository";
