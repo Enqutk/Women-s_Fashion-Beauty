9985db515c18e36806cc65c2e1be20ef69e249de
Enqutk <enkukokob@gmail.com>
2026-04-26 14:42:37 +0300

    Add order module exports in index.ts to streamline access to order-related functionalities, including routes, controllers, services, and repositories.

---
+++ server/src/modules/order/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./order.routes";
+export * as orderController from "./order.controller";
+export * as orderService from "./order.service";
+export * as orderRepository from "./order.repository";
