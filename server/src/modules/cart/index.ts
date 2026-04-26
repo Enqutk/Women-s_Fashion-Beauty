2478c1b3bcf5a9b2668f8eb02d53619a64514189
Enqutk <enkukokob@gmail.com>
2026-04-26 14:27:22 +0300

    Add index file in cart module to re-export routes, controller, service, and repository, streamlining imports and enhancing code organization.

---
+++ server/src/modules/cart/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./cart.routes";
+export * as cartController from "./cart.controller";
+export * as cartService from "./cart.service";
+export * as cartRepository from "./cart.repository";
