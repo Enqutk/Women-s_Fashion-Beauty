84eff3c89b527955671e8592208cceae9549bc42
Enqutk <enkukokob@gmail.com>
2026-04-26 14:28:42 +0300

    Add index file in category module to re-export routes, controller, service, and repository, improving import organization and module structure.

---
+++ server/src/modules/category/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./category.routes";
+export * as categoryController from "./category.controller";
+export * as categoryService from "./category.service";
+export * as categoryRepository from "./category.repository";
