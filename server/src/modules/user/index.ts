91fcba872c11b6f69c2fbd656020ab56a990d5b5
Enqutk <enkukokob@gmail.com>
2026-04-30 12:38:07 +0300

    Refactor user module exports; add userController export and streamline user.routes integration for improved module organization.

--- server/src/modules/user/index.ts
+++ server/src/modules/user/index.ts
@@ -1,3 +1,5 @@
+export { default } from "./user.routes";
 export * as userRepository from "./user.repository";
 export * as userService from "./user.service";
+export * as userController from "./user.controller";
 export * from "./user.model";
